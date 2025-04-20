64a44820838715d8e1745f0348fb62eee57b8094
Arjun Nair <arjun@warpstream.com>
2025-04-20 00:37:23 -0400

    begin work on add hold view + refactor

--- AddHoldView.swift
+++ AddHoldView.swift
@@ -4,9 +4,12 @@ import Foundation
 struct AddHoldView: View {
     var wall: Wall
 
+    // TODO: When finished adding a hold, navigate back to the wall.
+    // You must pass the wall back and recreate the view.
     var body: some View {
         Text("Add Hold View")
             .font(.largeTitle)
             .padding()
+            .navigationBarBackButtonHidden(true)
     }
 }
--- bored.climb/EditBoardView.swift
+++ bored.climb/EditBoardView.swift
@@ -8,7 +8,7 @@ struct EditBoardView: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(\.modelContext) private var context
 
-    @State private var navigateToAddHold = false
+    @State private var navigateToAddHoldView = false
 
     var body: some View {
         VStack(spacing: 0) {
@@ -38,6 +38,7 @@ struct EditBoardView: View {
             .background(Color(UIColor.systemGray6)) // Light toolbar background
             .overlay(Divider(), alignment: .bottom)
 
+            // TODO: Make sure that in all views, the image fits in the entire screen.
             if let data = imageData, let uiImage = UIImage(data: data) {
                 Image(uiImage: uiImage)
                     .resizable()
@@ -48,20 +49,38 @@ struct EditBoardView: View {
                     .padding()
             }
 
+            Button("Add Hold") {
+                // TODO: Wall shouldn't really be saved if there's literally no edits to it.
+                if wall == nil, let data = imageData {
+                    // Create a new wall with the image. Do not persist it yet.
+                    wall = Wall(imageData: data)
+                    context.insert(wall.unsafelyUnwrapped)
+                    saveContext(context: context)
+                }
+
+                // No need to reinsert, just and all the updates will automatically be saved.
+                saveContext(context: context)
+                navigateToAddHoldView = true
+            }
+            .padding(.top, 12)
+
             Spacer()
+            
+            // TODO: Render a list of all holds here. Have a delete button to delete the hold. Show
+            // a confirmation.
         }
         .onAppear {
             // TODO: Auto save the wall if any property of the wall is changed.
             if imageData == nil && wall != nil {
                 // Existing wall is being edited.
-            } else if wall == nil, let data = imageData {
+            } else if wall == nil, let _ = imageData {
                // This is ok, a new wall will be created using the imageData.
             } else {
                 // Both wall and image data are nil, or both are set.
                 fatalError("both wall and image data are nil or set")
             }
         }
-        .navigationDestination(isPresented: $navigateToAddHold) {
+        .navigationDestination(isPresented: $navigateToAddHoldView) {
             if let wall = wall {
                 AddHoldView(wall: wall)
             }
