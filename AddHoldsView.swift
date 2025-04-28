d835dc3988890f8293da34ad26e60855d573f11d
Arjun Nair <arjun@warpstream.com>
2025-04-27 20:48:20 -0400

    add models.swift

--- AddHoldsView.swift
+++ AddHoldsView.swift
@@ -2,29 +2,6 @@ import SwiftUI
 import SwiftData
 import Foundation
 
-@Model
-class Hold {
-    var points: [CGPoint] = []
-
-    init(points: [CGPoint]) {
-        self.points = points
-    }
-}
-
-@Model
-class Wall {
-    var imageData: Data
-    var width: CGFloat
-    var height: CGFloat
-    var holds: [Hold] = []
-
-    init(imageData: Data, width: CGFloat, height: CGFloat) {
-        self.imageData = imageData
-        self.width = width
-        self.height = height
-    }
-}
-
 struct AddHoldsView: View {
     @Environment(\.dismiss) private var dismiss
 
---
+++ models.swift
@@ -0,0 +1,25 @@
+import Foundation
+import SwiftData
+
+@Model
+class Hold {
+    var points: [CGPoint] = []
+
+    init(points: [CGPoint]) {
+        self.points = points
+    }
+}
+
+@Model
+class Wall {
+    var imageData: Data
+    var width: CGFloat
+    var height: CGFloat
+    var holds: [Hold] = []
+
+    init(imageData: Data, width: CGFloat, height: CGFloat) {
+        self.imageData = imageData
+        self.width = width
+        self.height = height
+    }
+}
